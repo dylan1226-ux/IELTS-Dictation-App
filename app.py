10c38751c683677a0e29630d7e6ffb1b0cf9a8dd
dylan1226-ux <dylanpan@usc.edu>
2024-12-06 16:32:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pandas as pd
 app = Flask(__name__)
 
 
-basic_words = pd.read_csv('/Users/dylanpan/Desktop/PycharmProjects/pythonProject1/words_basic.csv')
-advanced_words = pd.read_csv('/Users/dylanpan/Desktop/PycharmProjects/pythonProject1/words_advanced.csv')
+basic_words = pd.read_csv('words_basic.csv')
+advanced_words = pd.read_csv('words_advanced.csv')
 
 
 @app.route('/')
