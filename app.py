efdd622a76feb2382e414db2b7009f7c5baf1a37
dylan1226-ux <dylanpan@usc.edu>
2024-12-12 16:41:00 +0800

    update

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
-basic_words = pd.read_csv('words_basic.csv')
-advanced_words = pd.read_csv('words_advanced.csv')
+basic_words = pd.read_csv('/Users/dylanpan/Documents/GitHub/IELTS-Dictation-App/IELTS_vocab/words_basic.csv')
+advanced_words = pd.read_csv('/Users/dylanpan/Documents/GitHub/IELTS-Dictation-App/IELTS_vocab/words_advanced.csv')
+
+basic_words['code'] = basic_words['code'].astype(int)
+advanced_words['code'] = advanced_words['code'].astype(int)
 
 
 @app.route('/')
@@ -25,25 +28,35 @@ def start_practice():
 
     word_code_translation_pairs = [(row['code'], row['translation']) for _, row in words.iterrows()]
 
-    return render_template('practice.html', words=word_code_translation_pairs)
+    return render_template('practice.html', words=word_code_translation_pairs, word_list=word_list)
 
 
 @app.route('/submit_answers', methods=['POST'])
 def submit_answers():
     words = request.form
+    word_list = request.form['word_list']  # Get the word_list value from the form
+
     result = []
 
-    # collect user input
+    # Iterate over each submitted answer
     for code, user_input in words.items():
-        word_row = basic_words[basic_words['code'] == int(code)] if int(code) in basic_words['code'].values else \
-            advanced_words[advanced_words['code'] == int(code)]
+        # Skip the hidden field 'word_list'
+        if code == 'word_list':
+            continue
+
+        # Fetch the correct word row based on whether it's from the basic or advanced list
+        if word_list == 'basic':
+            word_row = basic_words[basic_words['code'] == int(code)]
+        else:
+            word_row = advanced_words[advanced_words['code'] == int(code)]
 
         correct_word = word_row['word'].values[0]
         chinese_translation = word_row['translation'].values[0]
-        result.append((code, user_input, correct_word, chinese_translation))
+        is_correct = user_input.strip().lower() == correct_word.strip().lower()
+
+        result.append((code, user_input, correct_word, chinese_translation,is_correct))
 
     return render_template('results.html', result=result)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
